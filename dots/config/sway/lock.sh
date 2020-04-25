dfe80b4531c6b2e4b33404ab3ec743b7e6cae229
Martin Dørum <martid0311@gmail.com>
2020-04-25 22:19:35 +0200

    colors

--- dots/config/sway/lock.sh
+++ dots/config/sway/lock.sh
@@ -3,8 +3,9 @@
 swaylock --screenshots --clock --indicator-idle-visible \
 	--indicator-radius 100 \
 	--indicator-thickness 7 \
-	--ring-color bb00cc \
-	--key-hl-color 880033 \
+	--ring-color 455a64 \
+	--key-hl-color be5046 \
+	--text-color ffc107 \
 	--line-color 00000000 \
 	--inside-color 00000088 \
 	--separator-color 00000000 \
