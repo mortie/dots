a74e0ab58c7626f4658ed00c15dc518e39b77dfb
Martin Dørum <martid0311@gmail.com>
2020-04-29 14:20:08 +0200

    cool new locking stuff

--- dots/config/sway/idle.sh
+++ dots/config/sway/idle.sh
@@ -4,4 +4,4 @@ swayidle \
 	timeout 60 '~/.config/sway/lock.sh --grace 10 --fade-in 4' \
 	timeout 80 'swaymsg "output * dpms off"' \
 	resume 'swaymsg "output * dpms on"' \
-	before-sleep '~/.config/sway/lock.sh'
+	before-sleep '~/.config/sway/lock.sh --fade-in 0'
--- dots/config/sway/lock.sh
+++ dots/config/sway/lock.sh
@@ -3,6 +3,7 @@
 swaylock --screenshots --clock --indicator-idle-visible \
 	--indicator-radius 100 \
 	--indicator-thickness 7 \
+	--ignore-empty-password \
 	--ring-color 455a64 \
 	--key-hl-color be5046 \
 	--text-color ffc107 \
@@ -14,4 +15,3 @@ swaylock --screenshots --clock --indicator-idle-visible \
 	--effect-vignette 0.5:0.5 \
 	--effect-compose "0,1.5%;-1x10%;$HOME/.config/sway/lock.svg" \
 	"$@"
-
