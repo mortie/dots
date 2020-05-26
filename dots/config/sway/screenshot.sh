72add471107b099d61d82c1fea4a67c53ec6b658
Martin Dørum <martid0311@gmail.com>
2020-05-26 09:15:13 +0200

    new screenshot scripts

--- dots/config/sway/screenshot.sh
+++ dots/config/sway/screenshot.sh
@@ -2,17 +2,52 @@
 
 set -e
 
-screenshot_path="$HOME/cloud/screenshots"
-base="screenshot-$(date +'%Y-%m-%dT%H:%M:%S').png"
-file="$screenshot_path/$base"
+host="https://p.mort.coffee"
 
-if [ "$1" = select ]; then
-	grim -g "$(slurp)" "$file"
-elif [ -z "$1" ]; then
-	grim "$file"
+shot() {
+	if [ -z "$1" ]; then
+		grim -
+	else
+		grim -g "$1" -
+	fi
+}
+
+compress() {
+	convert png:- jpeg:-
+}
+
+strip_nl() {
+	sed -z '$ s/\n$//'
+}
+
+selection=""
+if [ "$2" = select ]; then
+	selection="$(slurp)" || ret="$?"
+	if [ "$ret" = 1 ]; then
+		exit 0 # User probably hit esc to cancel
+	elif ! [ -z "$ret" ]; then
+		notify-send --urgency critical "Slurp command failed." "Exit code: $ret"
+		exit 1
+	fi
+fi
+
+if [ "$1" = upload ]; then
+	url="$(shot "$selection" | compress | curl --upload-file - "$host/screenshot.jpg")" || ret="$?"
+	if ! [ -z "$ret" ]; then
+		notify-send --urgency critical "Failed to capture" "Exit code: $ret"
+		exit 1
+	fi
+
+	echo "$url" | strip_nl | wl-copy
+elif [ "$1" = copy ]; then
+	shot "$selection" | wl-copy || ret="$?"
+	if ! [ -z "$ret" ]; then
+		notify-send --urgency critical "Failed to capture" "Exit code: $ret"
+		exit 1
+	fi
 else
-	echo "Usage: $0 [select]"
+	echo "Usage: $0 <upload|copy> [select]"
 	exit 1
 fi
 
-notify-send "Screenshot captured." "$base"
+
