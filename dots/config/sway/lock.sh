509070cba69a0a984fe583fd848617df8f0bdc92
Martin Dørum <martid0311@gmail.com>
2020-05-28 12:53:25 +0200

    big blur

--- dots/config/sway/lock.sh
+++ dots/config/sway/lock.sh
@@ -11,7 +11,7 @@ swaylock --screenshots --clock --indicator-idle-visible \
 	--inside-color 00000088 \
 	--separator-color 00000000 \
 	--fade-in 0.1 \
-	--effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
+	--effect-scale 0.5 --effect-blur 8x3 --effect-scale 2 \
 	--effect-vignette 0.5:0.5 \
 	--effect-compose "0,1.5%;-1x10%;$HOME/.config/sway/lock.svg" \
 	"$@"
