93d95db4ed5ca75e6ba0ff706dac9ee7d869ab07
Martin Dørum <martid0311@gmail.com>
2024-03-19 14:42:22 +0100

    whitespace changes in a script

--- dots/config/sway/scratchpad.sh
+++ dots/config/sway/scratchpad.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while :; do
-    kitty --class scratchpad
-    echo "Kitty exited with code $?, restarting" >&2
+	kitty --class scratchpad
+	echo "Kitty exited with code $?, restarting" >&2
 done
