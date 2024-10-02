4bfc4e5aed0f6570aa66fc77dca4beff67710178
Martin Dørum <martid0311@gmail.com>
2024-10-02 21:15:41 +0200

    update wezterm

--- dots/config/wezterm/wezterm.lua
+++ dots/config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local config = {}
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
@@ -8,6 +9,11 @@ config.font = wezterm.font "Fira Code"
 config.colors = {
 	foreground = "white",
 	background = "black",
+}
+
+config.keys = {
+  { key = '{', mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
+  { key = '}', mods = 'SHIFT|ALT', action = act.MoveTabRelative(1) },
 }
 
 return config
