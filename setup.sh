508700da88c7a49ecde7c20635b5617c885be430
Martin Dørum <martid0311@gmail.com>
2026-03-23 11:03:50 +0100

    set up flatpak gram config too

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 srcdir="$(pwd)/dots"
 destdir="$HOME"
 
+# Link file:
+# link from [to]
+# dots/$from -> $HOME/.$from
+# dots/$from -> $HOME/$to
 link() {
 	from="$srcdir/$1"
 	if [ -z "$2" ]; then
@@ -30,6 +34,26 @@ link() {
 	fi
 }
 
+# Link file to flatpak var dir:
+# flatlink ident from [to]
+# dots/$from -> $HOME/.var/app/$ident/$from
+# dots/$from -> $HOME/.var/app/$ident/$to
+flatlink() {
+	ident="$1"
+	from="$2"
+	if [ -z "$3" ]; then
+		to=".var/app/$ident/$2"
+	else
+		to=".var/app/$ident/$3"
+	fi
+
+	if [ -d "$destdir/.var/app/$ident" ]; then
+		link "$from" "$to"
+	else
+		echo "Skipping flatlink -> $ident/$to because $ident is not installed"
+	fi
+}
+
 link bashrc
 link zshrc
 link config/nvim/init.vim
@@ -41,7 +65,9 @@ link config/helix/config.toml
 link config/starstruck.lsp
 link config/wezterm/wezterm.lua
 link config/gram/keymap.jsonc
+flatlink app.liten.Gram config/gram/keymap.jsonc
 link config/gram/settings.jsonc
+flatlink app.liten.Gram config/gram/settings.jsonc
 link local/share/nvim/site/autoload/plug.vim
 link local/bin/pwrsave
 link local/bin/gdx
